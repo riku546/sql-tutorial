b333705e3515290978729af7b7cb779f047e8e84
riku <riku62560@gmail.com>
2024-06-25 23:08:00 +0900

    having

--- sample.sql
+++ sample.sql
@@ -17,3 +17,12 @@ SELECT 商品名 , sum(売上金額), count(*) , count(商品名)
 FROM test_table
 GROUP by 商品名
 ORDER by sum(売上金額)
+
+
+
+SELECT * 
+FROM test_table
+WHERE 商品名 != 'ニット'
+GROUP by 商品名
+HAVING sum(売上金額) > 10000
+ORDER by 売上金額
