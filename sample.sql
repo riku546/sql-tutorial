f6234d29f759dcfb667c76466b28fdfb91ca7001
riku546 <91833521+riku546@users.noreply.github.com>
2024-06-26 10:11:49 +0900

    between like

--- sample.sql
+++ sample.sql
@@ -1,3 +1,4 @@
+
 SELECT ID , 数量
 FROM test_table
 
@@ -11,3 +12,12 @@ SELECT *
 from test_table
 WHere ID < 10
 ORDER by 単価 DESC
+
+SELECT *
+from test_table
+WHERE 売上金額 Between 10000 and 200000
+
+
+SELECT *
+from test_table 
+WHERE 商品名 Like '%A%'
