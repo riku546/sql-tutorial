462f206883ff9f67334cf6574b5381781ae590c7
riku546 <91833521+riku546@users.noreply.github.com>
2024-11-14 18:35:52 +0900

    Update sample2.sql

--- sample2.sql
+++ sample2.sql
@@ -3,3 +3,4 @@ select shohin_bunrui , sum(hanbai_tanka) from shohin group by rollup(shohin_bunr
 select shohin_bunrui , torokubi , sum(hanbai_tanka) from shohin group by shohin_bunrui , torokubi order by shohin_bunrui;
 select case when grouping(shohin_bunrui) = 1 then '合計' else shohin_bunrui end , sum(hanbai_tanka) from shohin g
 roup by rollup(shohin_bunrui);
+select * from books;
