3651d263dc404e6e24cfc00a5ed9aaeb4307d3f8
riku546 <91833521+riku546@users.noreply.github.com>
2024-11-18 00:38:44 +0900

    Update sample2.sql

--- sample2.sql
+++ sample2.sql
@@ -47,3 +47,4 @@ select b.name from books as b inner join book_sales as bs on b.id = bs.book_id w
 select c.name, sum(price * figure) as sales from book_sales as bs inner join book_categories as bc on bs.book_id = bc.book_id inner join categories as c on bc.category_id = c.id group by c.id order by sales desc limit 3; 
 select a.name , count(*) as published_title_num from books as b inner join book_authors as ba on b.id = ba.book_id inner join authors as a on ba.author_id = a.id group by a.name order by published_title_num desc , a.name limit 3;
 select b.name from books as b  inner join book_categories as bc on b.id = bc.book_id inner join categories as c on bc.category_id = c.id group by b.name having count(b.name) >= 2;
+select b.name from books as b inner join book_authors as ba on b.id = ba.book_id inner join authors as a on ba.author_id = a.id where (b.name like '%宇宙%' or b.name like '%星%') and a.gender = '女性';
