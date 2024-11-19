5d5ad7e5b66e3c6da100b6cd9012dee3867afeed
riku546 <opa9788@gmail.com>
2024-11-20 00:45:03 +0900

    結合

--- sample2.sql
+++ sample2.sql
@@ -52,3 +52,4 @@ delete from events where id = 1;
 update events set max_num = 200 where id = 2;
 insert into events (id , name , max_num) values (3 , '古本まつり' , 75);
 select * from events;
+select coalesce(t.tenpo_id, '不明') , coalesce(t.tenpo_mei , '不明') , s.shohin_id , s.shohin_mei , s.hanbai_tanka from shohin as s left join tenpo  as t on s.shohin_id = t.shohin_id;
