b507ebd1d78a4a8304fd39456c822ae1daa43128
riku <opa9788@gmail.com>
2024-11-21 16:52:25 +0900

    サブクエリ

--- sample2.sql
+++ sample2.sql
@@ -55,4 +55,4 @@ select * from events;
 select coalesce(t.tenpo_id, '不明') , coalesce(t.tenpo_mei , '不明') , s.shohin_id , s.shohin_mei , s.hanbai_tanka from shohin as s left join tenpo  as t on s.shohin_id = t.shohin_id;
 select shohin_id , shohin_mei , shohin_bunrui , hanbai_tanka , (select avg(hanbai_tanka) from shohin) as hanbai_tanka_all from shohin;
 select shohin_id , shohin_mei , shohin_bunrui , hanbai_tanka , (select avg(hanbai_tanka) from shohin as s2 where s2.shohin_bunrui = s1.shohin_bunrui group by shohin_bunrui) as avg_hanbai_tanka from shohin as s1;
-
+select e1.name , e1.salary from employees as e1 where e1.salary >= (select avg(e2.salary) from employees as e2 where e1.department_id = e2.department_id group by department_id) as avg_salary;
